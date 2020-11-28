c3c9f3e159a6e72be3b64dd2c95108ecaa4ef4a7
Scovia Achan <achanscovia@gmail.com>
2020-11-29 00:13:51 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const authRoute = require("./routes/auth");
+
+dotenv.config();
+
+//connect to database
+mongoose.connect(process.env.DB_CONNECT, { useUnifiedTopology: true, useNewUrlParser: true, useCreateIndex: true },
+() => {console.log('database connected')}
+);
+
+//middleware
+app.use(express.json())
+
+app.use('/register/user', authRoute);
+
+
+
+app.listen(8090, ()=>{console.log("server is running")})
+
